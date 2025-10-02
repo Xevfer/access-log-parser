139c0655e1004a85224789bb8972cf3f47f6d0ae
Ivanov Egor <xefver@gmail.com>
2025-10-02 20:11:17 +0300

    add branch 2-number-experiment

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число");
+        int firstNumber = new Scanner (System.in).nextInt();
+        System.out.println("Введите второе число");
+        int secondNumber = new Scanner (System.in).nextInt();
+        double quotient = (double) firstNumber/ secondNumber;
+        String a="Сумма:";
+        String b="Разность:";
+        String c="Произведение:";
+        String d="Частное:";
+        System.out.println(a+(secondNumber+firstNumber));
+        System.out.println(b+(secondNumber-firstNumber));
+        System.out.println(c+(secondNumber*firstNumber));
+        System.out.println(d+quotient);
 
     }
 }
